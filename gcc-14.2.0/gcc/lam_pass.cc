e81e6ac7799c6cc9f262b95a92c8ff7f64956013
john <jwnhy0@gmail.com>
2024-09-30 16:43:47 +0800

    FUCK GCC

--- gcc-14.2.0/gcc/lam_pass.cc
+++ gcc-14.2.0/gcc/lam_pass.cc
@@ -34,6 +34,7 @@
 #include "target.h"
 #include "langhooks.h"
 #include "pretty-print.h"
+#include "gimple-pretty-print.h"
 
 namespace {
 const pass_data pass_data_lam = {
@@ -51,20 +52,64 @@ public:
   unsigned int execute(function *fun) final override;
 };
 
-void handle_integer_cst(pretty_printer *pp, gimple *stmt, tree *rhs) {
-  if (TREE_INT_CST_LOW(*rhs) & LAM_MASK)
-    error("LAM offset overflowed at %s:%d\n", gimple_filename(stmt),
-          gimple_lineno(stmt));
+bool is_special_cst(tree t) {
+  return TREE_CODE(t) == INTEGER_CST &&
+         ((TREE_INT_CST_LOW(t) & LAM_MASK) == LAM_MASK);
 }
 
-void handle_lam_component_ref(pretty_printer *pp, gimple *stmt, tree *rhs) {
-  tree off = component_ref_field_offset(*rhs);
-  if (TREE_CODE(off) == INTEGER_CST) {
-    handle_integer_cst(pp, stmt, &off);
-  } else {
-    pp_printf(pp, "Unhandled offset type %s\n",
-              get_tree_code_name(TREE_CODE(off)));
+void handle_integer_cst(gimple_stmt_iterator *gsi, tree *rhs,
+                        bool strict = true) {
+  gimple *stmt = gsi_stmt(*gsi);
+  if (!(TREE_INT_CST_LOW(*rhs) & LAM_MASK))
+    return;
+  if (strict) {
+    error("LAM offset overflowed with 0x%lx at %s:%d\n", TREE_INT_CST_LOW(*rhs),
+          gimple_filename(stmt), gimple_lineno(stmt));
   }
+}
+
+void handle_pointer_plus(gimple_stmt_iterator *gsi) {
+  gimple *stmt = gsi_stmt(*gsi);
+  tree lhs = gimple_assign_lhs(stmt);
+  tree lhstype = TREE_TYPE(lhs);
+  tree rhs1 = gimple_assign_rhs1(stmt);
+  tree llu_type = long_long_unsigned_type_node;
+
+  tree rhs_int_expr = fold_build1(NOP_EXPR, llu_type, rhs1);
+  tree rhs1_int = make_ssa_name(llu_type);
+  gimple *rhs_conv_stmt = gimple_build_assign(rhs1_int, rhs_int_expr);
+  gsi_insert_before(gsi, rhs_conv_stmt, GSI_SAME_STMT);
+
+  tree mask = build_int_cstu(llu_type, LAM_MASK);
+  tree save_node = make_ssa_name(llu_type);
+  tree save_expr = build2(BIT_AND_EXPR, llu_type, rhs1_int, mask);
+  gimple *save_stmt = gimple_build_assign(save_node, save_expr);
+  gsi_insert_before(gsi, save_stmt, GSI_SAME_STMT);
+
+  tree replaced_lhs = make_ssa_name(lhstype);
+  gimple_assign_set_lhs(stmt, replaced_lhs);
+
+  tree lhs_int_expr = fold_build1(NOP_EXPR, llu_type, replaced_lhs);
+  tree lhs_int = make_ssa_name(llu_type);
+  gimple *lhs_conv_stmt = gimple_build_assign(lhs_int, lhs_int_expr);
+  gsi_insert_after(gsi, lhs_conv_stmt, GSI_LAST_NEW_STMT);
+
+  tree inv_mask = build_int_cstu(llu_type, ~LAM_MASK);
+  tree clear_node = make_ssa_name(llu_type);
+  tree clear_expr = build2(BIT_AND_EXPR, llu_type, lhs_int, inv_mask);
+  gimple *clear_stmt = gimple_build_assign(clear_node, clear_expr);
+  gsi_insert_after(gsi, clear_stmt, GSI_LAST_NEW_STMT);
+
+  tree restore_expr = build2(BIT_IOR_EXPR, lhstype, clear_node, lhs_int);
+  tree lhs_int_ssa = make_ssa_name(llu_type);
+  gimple *restore_stmt = gimple_build_assign(lhs_int_ssa, restore_expr);
+  gsi_insert_after(gsi, restore_stmt, GSI_LAST_NEW_STMT);
+
+  tree lhs_ptr_expr = fold_build1(NOP_EXPR, lhstype, lhs_int);
+  gimple *lhs_ptr_stmt = gimple_build_assign(lhs, lhs_ptr_expr);
+  gsi_insert_after(gsi, lhs_ptr_stmt, GSI_LAST_NEW_STMT);
+
+  update_stmt(stmt);
 }
 
 void handle_lam_stmt(pretty_printer *pp, gimple_stmt_iterator *gsi) {
@@ -76,26 +121,27 @@ void handle_lam_stmt(pretty_printer *pp, gimple_stmt_iterator *gsi) {
       return;
     tree_code op = gimple_assign_rhs_code(stmt);
     tree rhs1 = gimple_assign_rhs1(stmt);
-    if (op == COMPONENT_REF) {
-      handle_lam_component_ref(pp, stmt, &rhs1);
-    } else if (op == INTEGER_CST) {
-      handle_integer_cst(pp, stmt, &rhs1);
-    } else if (op == CALL_EXPR) {
-
-    } else if (op == SSA_NAME) {
-      // SSA_NAME must a valid pointer type.
+    if (op == INTEGER_CST) {
+      // not a special constant, e.g., -1
+      if (!is_special_cst(rhs1))
+        handle_integer_cst(gsi, &rhs1);
+    } else if (op == POINTER_PLUS_EXPR) {
+      handle_pointer_plus(gsi);
+    } else if (op == ADDR_EXPR) {
+      tree inner = TREE_OPERAND(rhs1, 0);
+    } else if (op == CALL_EXPR || op == SSA_NAME || op == COMPONENT_REF ||
+               op == POINTER_DIFF_EXPR) {
       return;
     } else {
       pp_printf(pp, "Unhandled RHS%s \n", get_tree_code_name(op));
     }
-
   } else if (is_gimple_call(stmt)) {
 
   } else if (gimple_code(stmt) == GIMPLE_PHI) {
     // PHI node must be a valid pointer type.
     return;
   } else {
-    pp_printf(pp, "Unhandled stmt %s\n", gimple_code_name[gimple_code(stmt)]);
+    pp_printf(pp, "Unhandled stmt %s \n", gimple_code_name[gimple_code(stmt)]);
   }
 }
 
--- nginx-1.26.2/test.c
+++ nginx-1.26.2/test.c
@@ -2,7 +2,7 @@ int main() {
   int i = 10;
   int *a = 0;
   if (i == 0) {
-    a = (void *)(1UL<<47);
+    a = (void *)(1UL << 47);
   } else if (i == 10) {
     a = (void *)2;
   }
