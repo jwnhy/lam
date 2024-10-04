3716863d575c38f282c4d9148207225fe5495f9f
jwnhy <jwnhy@gmail.com>
2024-10-04 14:58:36 +0800

    update

--- llvm_lam/pass_lam/PassLam.cpp
+++ llvm_lam/pass_lam/PassLam.cpp
@@ -1,28 +1,41 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
 
-class LamModPass : public ModulePass {
+class LamModPass : public PassInfoMixin<LamModPass> {
 public:
-  LamModPass() : ModulePass(ID) {}
+  PreservedAnalyses run(Module &M, ModulePassManager &) {
+    errs() << M.getName() << "\n";
+    return PreservedAnalyses::all();
+  }
 
-  bool runOnModule(Module &M) override;
-
-  static char ID;
+  static bool isRequired() { return true; }
 };
+
 } // namespace
-
-char LamModPass::ID = 0;
-
-bool LamModPass::runOnModule(Module &M) {
-  errs() << M.getName() << "\n";
-  return false;
+  //
+llvm::PassPluginLibraryInfo getLamModPassInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "LamMod", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, ModulePassManager &MPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "lam-mod") {
+                    MPM.addPass(LamModPass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
 }
 
-static RegisterPass<LamModPass> RegLamModPass("lammod",
-                                              "LAM Module Pass", false, false);
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
+  return getLamModPassInfo();
+}
