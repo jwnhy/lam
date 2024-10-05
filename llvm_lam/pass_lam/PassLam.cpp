446b075f4f482e2442d8030490c93c05e7ff6ad2
john <jwnhy0@gmail.com>
2024-10-05 13:13:58 +0800

    update

--- llvm_lam/pass_lam/PassLam.cpp
+++ llvm_lam/pass_lam/PassLam.cpp
@@ -1,3 +1,4 @@
+#include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Pass.h"
@@ -8,27 +9,34 @@
 using namespace llvm;
 
 namespace {
-
-class LamModPass : public PassInfoMixin<LamModPass> {
+class LamModulePass : public PassInfoMixin<LamModulePass> {
 public:
-  PreservedAnalyses run(Module &M, ModulePassManager &) {
-    errs() << M.getName() << "\n";
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
+    for (Function &F : M) {
+      if (F.isDeclaration())
+        continue;
+      errs() << "Function: " << F.getName() << "\n";
+      for (BasicBlock &BB : F) {
+        for (Instruction &I : BB) {
+          errs() << "  " << I << "\n";
+        }
+      }
+    }
     return PreservedAnalyses::all();
   }
 
   static bool isRequired() { return true; }
 };
-
 } // namespace
   //
-llvm::PassPluginLibraryInfo getLamModPassInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "LamMod", LLVM_VERSION_STRING,
+llvm::PassPluginLibraryInfo getLamPassInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "LAM", LLVM_VERSION_STRING,
           [](PassBuilder &PB) {
             PB.registerPipelineParsingCallback(
                 [](StringRef Name, ModulePassManager &MPM,
                    ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "lam-mod") {
-                    MPM.addPass(LamModPass());
+                  if (Name == "lam-module") {
+                    MPM.addPass(LamModulePass());
                     return true;
                   }
                   return false;
@@ -36,6 +44,7 @@ llvm::PassPluginLibraryInfo getLamModPassInfo() {
           }};
 }
 
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return getLamModPassInfo();
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getLamPassInfo();
 }
